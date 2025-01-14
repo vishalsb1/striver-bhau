01db068dff7c010ccdef836ed5de96b736316f83
VISHAL SUBHASH BIRADAR <143383723+vishalsb1@users.noreply.github.com>
2025-01-14 15:59:51 +0000

    add merge sort

--- 2_sorting/sorting.cpp
+++ 2_sorting/sorting.cpp
@@ -51,6 +51,63 @@ void print(int arr[],int n){
     cout<<endl;
     cout<<endl;
 }
+
+// merege sorting algo
+void merge_arr(int arr[],int low,int mid,int high){
+
+    // making an temp array
+    vector <int> temp;
+    // left part array cha pahila index
+    int left_ptr=low;
+    // right part array cha pahila index
+    int right_ptr=mid+1;
+
+
+    // if left and right aarray lengths are same this will be okay ,
+    // but if the left side arrray is larger or smaller wise verses(right side) 
+    while(left_ptr<=mid && right_ptr<=high ){
+        if(arr[left_ptr]<=arr[right_ptr]){
+            temp.push_back(arr[left_ptr++]);
+        }else temp.push_back(arr[right_ptr++]);
+    }
+    // then the below code will help 
+
+    // if right arr size is greater
+    while(right_ptr<=high){
+        temp.push_back(arr[right_ptr++]);
+    }
+
+    // if left arr size is greater
+    while(left_ptr<=mid){
+        temp.push_back(arr[left_ptr++]);
+    }
+
+    for(int i=0;i<temp.size();i++){
+        // star star // apan hai visru sakto 
+        // hai help karta aplela arrau chya lower index in solve karay 
+        // samja jar low=4 ani high =6 tr tya anusar te temp madle variable hai tyach array cha tyach index la theval
+        arr[low+i]=temp.at(i);
+    }
+}
+// 1st
+void merge_sort(int arr[],int low,int high){
+    // base case
+    // single element in array casse
+    if(low>=high) return;
+
+    int mid=(low+high)/2;
+
+    // hya index mi divide karaycha 
+    // pahile left
+    merge_sort(arr,low,mid);
+
+    // 2nd ->right side divide karaychi
+    merge_sort(arr,mid+1,high);
+
+    // merge karaycha logic wala function
+    merge_arr(arr,low,mid,high);
+}
+
 int main(){
     int arr[]={2,43,54,33,22,11};
     int n=sizeof(arr)/sizeof(int);
@@ -60,8 +117,8 @@ int main(){
     // sorting func
     // selection_sort(arr,n);
     // bubble_sort(arr,n);
-    insertion_sort(arr,n);
-
+    // insertion_sort(arr,n);
+    merge_sort(arr,0,n-1);
     cout<<"After swaping :";
     print(arr,n);
 
